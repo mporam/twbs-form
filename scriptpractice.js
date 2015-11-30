1ead7a0d7289fbfe74490978269fb6b8570e4f1d
charliecog <charlie.coggans@mayden.co.uk>
2015-11-30 16:21:46 +0000

    onelinewonder

--- scriptpractice.js
+++ scriptpractice.js
@@ -1,11 +1,20 @@
+/*
+    //document.getElementsByClassName('form-group')[0].innerHTML = "hahahahahahaha"
+    $('.form-group').html("hahahahaha")
+
+    //document.getElementsByTagName('label')[0].innerHTML = "oh laaaawdy"
+    $('label').text("helloo")
+
+    //document.getElementsByName('charlie')[0].innerHTML = "mmmmmmm honeychild"
+    $('[name=charlie]').html("<p>fo sho</p>")
+
+    //document.getElementById('feet').innerHTML = "girrl you looking fly!"
+    $('#feet').html("pepepepepe")
+*/
+
 $(function() {
 
-    document.getElementsByClassName('form-group')[0].innerHTML = "hahahahahahaha"
-
-    document.getElementsByTagName('label')[0].innerHTML = "oh laaaawdy"
-
-    document.getElementsByName('charlie')[0].innerHTML = "mmmmmmm honeychild"
-
-    document.getElementById('feet').innerHTML = "girrl you looking fly!"
+    $('.form-group,.radio').not('#button').append("<p>a new pokemon has appeared</p>")
 
 })
+
