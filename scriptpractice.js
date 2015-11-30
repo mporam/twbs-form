9a2784922886a2d3b534c93c5bb5f24232195ccf
charliecog <charlie.coggans@mayden.co.uk>
2015-11-30 14:52:13 +0000

    adding files

---
+++ scriptpractice.js
@@ -0,0 +1,11 @@
+$(function() {
+
+    document.getElementsByClassName('form-group')[0].innerHTML = "hahahahahahaha"
+
+    document.getElementsByTagName('label')[0].innerHTML = "oh laaaawdy"
+
+    document.getElementsByName('charlie')[0].innerHTML = "mmmmmmm honeychild"
+
+    document.getElementById('feet').innerHTML = "girrl you looking fly!"
+
+})
