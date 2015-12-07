c2f48f03d377fa1432645eca0995ff01f558688e
Marta Pleszynska <marta.pleszynska@mayden.co.uk>
2015-12-07 20:35:09 +0000

    basicaly forgot to do my commits earlier so there is a bunch of new functions to validate the fields and some fixes in index.html, other files just got some random stuff from excercises

--- script.js
+++ script.js
@@ -29,58 +29,110 @@
 //    $('.form-group').not('#button').append("<div>This is a feedback message</div>")
 //
 //})
-$(function() {
+//$(function() {
+//
+//    $('[type=email]').focus(whenInFocus)
+//    $('[type=email]').focusout(whenInFocus)
+//    $('[type=email]').keyup(whenInFocus)
+//    //$('[type=email]').change(whenInFocus)
+//    $('[type=email]').blur(changeBackground)
+//    $('#letters').click(clicked)
+//
+//    //$('[type=email]').on("focus",whenInFocus)
+//    //$('[type=email]').on("blur",changeBackground)
+//
+//    $('form').submit(checkIfEmpty)
+//    $('form').submit(triggerFocusOnEmail)
+//    $('form').on('marta',triggerFocusOnEmail)
+//    $('form').submit(submitForm)
+//})
+//function submitForm() {
+//    alert ('You have pressed submit button!!!')
+//    $('form').trigger('marta')
+//    return false
+//}
+//function whenInFocus() {
+//    var value = 'Enter your email here...'
+//    $('[type=email]').attr("placeholder",value)
+//    changeBorder()
+//}
+//function changeBorder() {
+//    if ($('[type=email]').val()) {
+//        $('[type=email]').css("box-shadow", "inset 0 2px 2px rgba(0,0,0,.075),0 0 8px rgba(77, 255, 136,.8)")
+//        $('[type=email]').css("border-color", "#00802b")
+//    } else {
+//        //$('[type=email]').css("background-color", "red")
+//        $('[type=email]').css("box-shadow", "inset 0 2px 2px rgba(0,0,0,.075),0 0 8px rgba(255, 102, 102,.8)")
+//        $('[type=email]').css("border-color", "#b30000")
+//    }
+//
+//}
+//function checkIfEmpty() {
+//    var textType = '[type=text]'
+//    //var value = $(textType).val()
+//    if(!$(textType).val()){
+//        //console.log('false')
+//        $(textType).attr("placeholder","Enter something here")
+//
+//        return false
+//    }
+//}
+//function clicked () {
+//    $('#letters').attr("value","letters only!!!")
+//}
+//function triggerFocusOnEmail() {
+//    $('[type=email]').trigger("focus")
+//}
+$(function () {
+    //$('.form-group').children('input').each(function () {
+    //    console.log(this)
+    //})
 
-    $('[type=email]').focus(whenInFocus)
-    $('[type=email]').focusout(whenInFocus)
-    $('[type=email]').keyup(whenInFocus)
-    //$('[type=email]').change(whenInFocus)
-    $('[type=email]').blur(changeBackground)
-    $('#letters').click(clicked)
+    //$('form').prop('elements').each(function() {
+    //    console.log(this)
+    ////})
+    $('.form-group').each(function () {
+        console.log($(this).find('input'))
+    })
+    //
+    //$('input').each(function () {
+    //    console.log(this)
+    //})
+    $('.form-group').each(function () {
+        console.log($('input'), this)
+    })
 
-    //$('[type=email]').on("focus",whenInFocus)
-    //$('[type=email]').on("blur",changeBackground)
-
-    $('form').submit(checkIfEmpty)
-    $('form').submit(triggerFocusOnEmail)
-    $('form').on('marta',triggerFocusOnEmail)
-    $('form').submit(submitForm)
 })
-function submitForm() {
-    alert ('You have pressed submit button!!!')
-    $('form').trigger('marta')
-    return false
-}
-function whenInFocus() {
-    var value = 'Enter your email here...'
-    $('[type=email]').attr("placeholder",value)
-    changeBorder()
-}
-function changeBorder() {
-    if ($('[type=email]').val()) {
-        $('[type=email]').css("box-shadow", "inset 0 2px 2px rgba(0,0,0,.075),0 0 8px rgba(77, 255, 136,.8)")
-        $('[type=email]').css("border-color", "#00802b")
-    } else {
-        //$('[type=email]').css("background-color", "red")
-        $('[type=email]').css("box-shadow", "inset 0 2px 2px rgba(0,0,0,.075),0 0 8px rgba(255, 102, 102,.8)")
-        $('[type=email]').css("border-color", "#b30000")
-    }
-
-}
-function checkIfEmpty() {
-    var textType = '[type=text]'
-    //var value = $(textType).val()
-    if(!$(textType).val()){
-        //console.log('false')
-        $(textType).attr("placeholder","Enter something here")
-
+function checkLengthMax8() {
+    var length = $($(this)).val().length
+    console.log(length)
+    if (length < 0) {
+        changeBorderColor('red',$(this))
+        displayMessage($(this), 'Not enough characters')
+        return false
+    } else if (length > 8) {
+        changeBorderColor('red',$(this))
+        displayMessage($(this), 'Too many characters')
         return false
     }
+    else {
+        changeBorderColor('green', $(this))
+        $('.msg').remove()
+    }
+}function checkLengthMin10Max25() {
+    var length = $($(this)).val().length
+    console.log(length)
+    if (length < 10) {
+        changeBorderColor('red',$(this))
+        displayMessage($(this), 'Not enough characters')
+        return false
+    } else if (length > 25) {
+        changeBorderColor('red',$(this))
+        displayMessage($(this), 'Too many characters')
+        return false
+    }
+    else {
+        changeBorderColor('green', $(this))
+        $('.msg').remove()
+    }
 }
-function clicked () {
-    $('#letters').attr("value","letters only!!!")
-}
-function triggerFocusOnEmail() {
-    $('[type=email]').trigger("focus")
-}
-
--- validation.js
+++ validation.js
@@ -1,37 +1,174 @@
 /**
- * Created by martapleszynska on 01/12/15.
+ * changes border color to red or green for specified element
+ * @param   String   color     'red' for red border, 'green' for green border'
+ * @param   String   element   html element selector
  */
-var required = '#one'
+function changeBorderColor(color, element) {
+    if (color == 'red') {
+        $(element).css({
+            "box-shadow": "inset 0 2px 2px rgba(0,0,0,.075),0 0 8px rgba(255, 102, 102,.8)",
+            "border-color": "#b30000"
+        })
+    }
+    if (color == 'green') {
+        $(element).css({
+            "box-shadow": "inset 0 2px 2px rgba(0,0,0,.075),0 0 8px rgba(77, 255, 136,.8)",
+            "border-color": "#00802b"
+        })
+    }
+}
+/**
+ * removes previous message if exists and insert a new message
+ * @param   String   element   html element selector
+ * @param   String   message   message to display
+ */
+function displayMessage(element, message) {
+    hideMessage(element)
+    $(element).after("<p class=\"msg\">" + message + "</p>")
+}
 
-//if empty but required or invalid input
-function changeBorderToRedIfInvalid(inputValue){
-    if (!$(required).val()){
-        $('[type=email]').css("box-shadow", "inset 0 2px 2px rgba(0,0,0,.075),0 0 8px rgba(255, 102, 102,.8)")
-        $('[type=email]').css("border-color", "#b30000")
-    }
+/**
+ *hides existing message
+ * @param   String   element   html element selector
+ */
+function hideMessage(element) {
+    $(element).parent('.form-group').children('.msg').remove()
 }
-//if correct input
-function changeBorderToGreenIfValid(inputValue){
-    if (!$(required).val()){
-        $('[type=email]').css("box-shadow", "inset 0 2px 2px rgba(0,0,0,.075),0 0 8px rgba(77, 255, 136,.8)")
-        $('[type=email]').css("border-color", "#00802b")
-    }
-}
-//value max length of 8
-function checkLength8 (inputValue){
-    if(inputValue.length <= 8) {
+/**
+ * checks the required field has and input value and changes the border appropriately and displays appropriate message
+ * @param     String   element   html element selector
+ * @returns   bool               false in invalid input
+ */
+function checkRequired(element) {
+    var value = $(element).val()
+    if (value) {
+        changeBorderColor('green', element)
+        hideMessage(element)
+    } else {
+        changeBorderColor('red', element)
+        displayMessage(element, 'Please fill this required field')
         return false
     }
 }
-//value length 10-25
-function checkLength10To25 (inputValue){
-    if(inputValue.length < 10 || inputValue.length > 25) {
-        //do something
+/**
+ * check if the input has a valid length of characters and changes the border appropriately and displays appropriate message
+ * @param    Integer   min        minimum valid number of characters allowed
+ * @param    Integer   max        maximum valid number of characters allowed
+ * @param    String    element    html element selector
+ * @returns  bool                 false in invalid input
+ */
+function checkLength(min, max, element) {
+    var inputLength = $(element).val().length
+    if (inputLength > 0) {
+        if (inputLength < min) {
+            changeBorderColor('red', element)
+            displayMessage(element, 'Not enough characters')
+            return false
+        } else if (inputLength > max) {
+            changeBorderColor('red', element)
+            displayMessage(element, 'Too many characters')
+            return false
+        }
+        else {
+            changeBorderColor('green', element)
+            hideMessage(element)
+        }
+    }
+}
+/**
+ * check if the input contains letters only and changes the border appropriately and displays appropriate message
+ * @param    String   element    html element selector
+ * @returns  bool                false if contains invalid characters
+ */
+function checkLetterOnly(element) {
+    var lettersOnly = /^[a-zA-Z]+$/
+    var inputValue = $(element).val()
+    console.log(inputValue)
+    if (lettersOnly.test(inputValue)) {
+        changeBorderColor('green', element)
+        hideMessage(element)
+    } else {
+        changeBorderColor('red', element)
+        displayMessage(element, 'Only letters allowed!')
         return false
     }
 }
+/**
+ * check if the input is a valid email address and changes the border appropriately and displays appropriate message
+ * @param    String   element    html element selector
+ * @returns  boolean             false if input is not a valid email address
+ */
+function checkEmail(element) {
+    var emailString = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/igm
+    var inputValue = $(element).val()
+    if (emailString.test(inputValue)) {
+        changeBorderColor('green', element)
+        hideMessage(element)
+    } else {
+        changeBorderColor('red', element)
+        displayMessage(element, 'This is not a valid email address!')
+        return false
+    }
+}
+$(function () {
+    $('#f1').blur(function () {
+        checkRequired('#f1')
+    })
+    $('#f2').blur(function () {
+        checkLength(0, 8, '#f2')
+    })
+    $('#f3').blur(function () {
+        if ($('#f3').val()) {
+            checkLength(10, 25, '#f3')
+        } else {
+            checkRequired('#f3')
+        }
+    })
+    $('#f4').blur(function () {
+        if ($('#f4').val()) {
+            checkLength(10, 25, '#f4')
+        }
+    })
+    $('#f5').blur(function () {
+        if ($('#f5').val()) {
+            checkLetterOnly('#f5')
+        }
+    })
+    $('#f6').blur(function () {
+        if ($('[type=radio]').prop('checked')) {
+            checkRequired('#f6')
+        }
+    })
+    $('#f7').blur(function () {
+        if ($('#f7').val()) {
+            checkEmail('#f7')
+        }
+    })
+    $('form').submit(function () {
+        checkRequired('#f1')
+        console.log(checkRequired('#f1'))
 
+        checkLength(0, 8, '#f2')
+        if ($('#f3').val()) {
+            checkLength(10, 25, '#f3')
+        } else {
+            checkRequired('#f3')
+        }
+        $('#f4').blur(function () {
+            if ($('#f4').val()) {
+                checkLength(10, 25, '#f4')
+            }
+        })
+        if ($('#f5').val()) {
+            checkLetterOnly('#f5')
+        }
+        if ($('[type=radio]').prop('checked')) {
+            checkRequired('#f6')
+        }
+        if ($('#f7').val()) {
+            checkEmail('#f7')
+        }
 
-   if ($(element).prop('checked')) {
-       //required
-   }
+    })
+
+})
